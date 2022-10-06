61063745de2b4b7028d7245aa3cd837389a767be
Sphiwe8 <109581436+Sphiwe8@users.noreply.github.com>
2022-10-06 12:30:54 +0200

    auth-changes

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './componets/Login';
+import Signup from './componets/Signup';
+import Welcome from './componets/WelcomePage';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Login />
+     
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +18,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
 });
---
+++ componets/Login.js
@@ -0,0 +1,154 @@
+import * as React from 'react';
+import { 
+ TextInput,
+ View, 
+ StyleSheet, 
+ Image,
+ Text,
+ TouchableOpacity,
+ } from 'react-native';
+
+export default function Login() {
+
+   const [text, onChangeText] = React.useState("");
+  const [password, onChangePassword] = React.useState(null);
+
+
+  return (
+    <View style={styles.container}>
+    <Image style={styles.logo} source={require('../assets/tiny.png')} />
+
+
+    <Text style={styles.header} >
+    Login
+    </Text>
+
+    <View style={styles.inputs} >
+
+    
+
+<View style={styles.username} >
+
+<Text style={styles.user} >
+    Email Address
+    </Text>
+
+     <TextInput
+       placeholder="Enter Email"
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+      />
+
+      </View>
+
+      
+      <View style={styles.username} >
+      <Text style={styles.user} >
+    Enter Password
+    </Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangePassword}
+        value={password}
+        placeholder="Password"
+        keyboardType="password"
+      />
+
+      </View>
+      
+      </View>
+
+      
+
+       <TouchableOpacity style={styles.button1}>      
+<Text style={styles.buttonText1}>
+      Log in
+      </Text>
+</TouchableOpacity>
+
+
+      
+      
+    </View>
+  );
+} 
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent:'center',
+    
+    height:'100%',
+    
+    
+  },
+  
+  logo: {
+    alignSelf:'center',
+    height: 80,
+    width: 180,
+    marginTop:25,
+    
+  },
+
+  header: {
+    textAlign: 'center',
+    fontSize: 28,
+    fontWeight: '500',
+    color: '#2FA4FF',
+    
+    
+
+  },
+
+  user:{
+    fontSize: 17,
+    fontWeight: '300',
+    paddingLeft: 45,
+
+
+  },
+
+  inputs:{
+    marginTop:40,
+  },
+
+    button1: {
+    margin: 20,
+width: 220,
+height: 50,
+backgroundColor:'#2FA4FF',
+alignSelf:'center',
+marginTop:60,
+borderRadius:20,
+
+  },
+
+    buttonText1:{
+    textAlign: 'center',
+    alignSelf:'center',
+    marginTop: 8,
+    fontSize: 22,
+    fontWeight:'500',
+    color:'#fff'
+  },
+
+  username:{
+marginTop:30,
+  },
+
+     input: {
+    height: 50,
+    margin: 12,
+    width:'80%',
+    borderWidth: 1,
+    padding: 10,
+    borderColor: '#2fa4ff',
+    alignSelf:'center',
+  },
+
+
+
+
+  
+});
