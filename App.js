74fd1ed226d7672bd7145123617e534b28d126fb
Lebo-hash30 <abrahamslebogang43@gmail.com>
2022-10-06 15:08:50 +0200

    soundChanged

--- App.js
+++ App.js
@@ -3,17 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 import Login from './componets/Login';
 import Signup from './componets/Signup';
 import Welcome from './componets/WelcomePage';
-<<<<<<< HEAD
-import Sounds from './componets/Sound';
-import Quotes from './componets/MotiveQuotes';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import Screen1 from './components/Screen1';
-=======
 import Emotions from './componets/Emotions';
 import Cause from './componets/Cause';
->>>>>>> c7eb6201ff70811cc74034d46479fd4fb83ea277
 
 
 
@@ -36,25 +27,11 @@ function DrawerRoutes() {
 }
 export default function App() {
   return (
-<<<<<<< HEAD
-    <NavigationContainer>
-    <Stack.Navigator>
-    
-      
-          <Stack.Screen name="sounds" component={DrawerRoutes} />
-            <Stack.Screen name="quotes" component={Quotes} />
-            <Stack.Screen name="" component={DrawerRoutes} />
-       
-         
-    </Stack.Navigator>
-  </NavigationContainer>
-=======
     <View style={styles.container}>
       <Cause />
      
       <StatusBar style="auto" />
     </View>
->>>>>>> c7eb6201ff70811cc74034d46479fd4fb83ea277
   );
 }
 
