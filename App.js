3958445a66f90c6f67c09121ef3ba4426a3f6d0d
Sphiwe8 <109581436+Sphiwe8@users.noreply.github.com>
2022-10-12 11:06:18 +0200

    Newchanges

--- App.js
+++ App.js
@@ -1,41 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
-import Login from './componets/Login';
-import Signup from './componets/Signup';
-import Welcome from './componets/WelcomePage';
-import Emotions from './componets/Emotions';
-import Cause from './componets/Cause';
-import Quotes from './componets/MotiveQuotes';
-
-const Drawer = createDrawerNavigator();
+import Welcome from './components/WelcomePage';
 
 
-function DrawerRoutes() {
-  return (
-    <Drawer.Navigator useLegacyImplementation>
 
-      <Drawer.Screen name="" component={Sound} />
-      <Drawer.Screen name="Stores" component={Screen1} />
-     
-    </Drawer.Navigator>
 
-  );
-}
+
 
 
 export default function App() {
   return (
-    <NavigationContainer>
-    <Stack.Navigator>
-    
-      
-          <Stack.Screen name="sounds" component={DrawerRoutes} />
 
-            <Stack.Screen name="" component={DrawerRoutes} />
-       
+    
+    <View>
+      <Welcome />
+    </View>
          
-    </Stack.Navigator>
-  </NavigationContainer>
+
 );
 }
 const styles = StyleSheet.create({
