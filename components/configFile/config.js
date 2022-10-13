3bae4d182f13145e3249f312368735e213ceed26
Basil Meyer <basilmeyer1985@gmail.com>
2022-10-13 12:09:06 +0200

    changes

---
+++ components/configFile/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDDIK4unPtc54jHMm0_Lo1iI5we95Ktyzk",
+  authDomain: "emospaceapp.firebaseapp.com",
+  projectId: "emospaceapp",
+  storageBucket: "emospaceapp.appspot.com",
+  messagingSenderId: "912059296421",
+  appId: "1:912059296421:web:fd5a8890c0f27583343b4c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
